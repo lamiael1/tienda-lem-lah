b3e0eda84d7f184a78740d346874430cae9c7ee1
Laura7229 <laura.alvarez4m7@gmail.com>
2026-04-14 12:46:31 +0200

    creacion categoria

---
+++ tienda-lem-lah-common/src/main/java/org/example/tiendalemlah/common/entities/Categoria.java
@@ -0,0 +1,8 @@
+package org.example.tiendalemlah.common.entities;
+
+public class Categoria {
+    private Long id;
+    private String nombre;
+    private String descripcion;
+    private String imagen;
+}
