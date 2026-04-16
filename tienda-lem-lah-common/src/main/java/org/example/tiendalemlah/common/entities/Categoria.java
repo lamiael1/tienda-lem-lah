2006d2d38e65b539b8f8c217bee16510c4098a5b
Laura7229 <laura.alvarez4m7@gmail.com>
2026-04-16 08:44:29 +0200

    commit 9

--- tienda-lem-lah-common/src/main/java/org/example/tiendalemlah/common/entities/Categoria.java
+++ tienda-lem-lah-common/src/main/java/org/example/tiendalemlah/common/entities/Categoria.java
@@ -1,6 +1,10 @@
 package org.example.tiendalemlah.common.entities;
 
-public class Categoria {
+import lombok.Data;
+
+@Data
+public class Category {
+
     private Long id;
     private String nombre;
     private String descripcion;
