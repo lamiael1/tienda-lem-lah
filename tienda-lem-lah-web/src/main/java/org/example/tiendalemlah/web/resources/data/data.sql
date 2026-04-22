4e83da9902a649aa45cf75ccdefc4adbef31b3cf
Lamia <lamiae.el1@educa.madrid.org>
2026-04-22 13:20:07 +0200

    data.sql creada con insert

---
+++ tienda-lem-lah-web/src/main/java/org/example/tiendalemlah/web/resources/data/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO categorias (nombre, descripcion, imagen, tipo) VALUES
+                                                               ('Organic Chicken', 'CANINO · ORGÁNICO', 'Organic.jpg', 'perros'),
+                                                               ('Venison & Truffle', 'CANINO · GOURMET', 'venisonTruffle.jpg', 'perros'),
+                                                               ('Gourmet Feast', 'FELINO · GOURMET', 'GorumetFeast.jpg', 'gatos'),
+                                                               ('Salmon & Caviar', 'FELINO · PREMIUM', 'SalmonCaviar.jpg', 'gatos');
